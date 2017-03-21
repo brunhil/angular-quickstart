d199c27789cf8428b8fdefae2de6e775267af0ec
brunhil <jack.batzner@gmail.com>
2017-03-21 18:40:31 -0500

    Finished Chapter 5, Services, of Angular.io tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,6 @@ import { HeroDetailComponent } from './hero-detail.component';
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent, HeroDetailComponent ],
-  bootstrap:    [ AppComponent ]
+  bootstrap:    [ AppComponent ],
 })
 export class AppModule { }
